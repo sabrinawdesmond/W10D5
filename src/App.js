c55de7cdbc0b7f9646d1b41c72359066261d0091
Olga <olga.sleepless@gmail.com>
2023-03-17 15:18:49 -0400

    Create harvardArt data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import HarvardArt from './data/harvardArt';
+
 function App() {
+  // console.log(HarvardArt);
   return (
     <h1>Hello from App</h1>
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+
+const Root = (props) => {
+  return (
+    <h1>Hello from Root</h1> 
+  );
+}
+// ReactDOM.createRoot(document.getElementById('root'));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </ BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
